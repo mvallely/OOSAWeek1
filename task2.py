06d2dce4d9f3d90460433ca6e7a43ddf47acf653
Mark Vallely <s2134833@baltic10.geos.ed.ac.uk>
2021-01-13 09:17:09 +0000

    Completed Task 2

---
+++ task2.py
@@ -0,0 +1,11 @@
+#OOSA Week 1 - Task 2 - Mark Vallely - s2134833
+
+# Nested loop - examine every square of chessboard
+# Create coordinate lists (x-axis, y-axis)
+x =[1,2,3,4,5,6,7,8]
+y =[1,2,3,4,5,6,7,8]
+
+# Nested for loop to count every coordinate option
+for a in x:
+    for b in y:
+        print(a,b)
